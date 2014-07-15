2d8405dcaf6152afc793d9b99371a171b4a0a18f
Ejla <ejla1@me.com>
2014-07-15 21:00:17 +0200

    Image helper

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,2 +1,9 @@
 module ImagesHelper
+  def image_tag(image, size=:default)
+    return 'no image' if image.blank?
+    width = 100 if size == :default
+    width = 300 if size == :large
+
+    tag(:img, src: "data:image/jpg;base64, #{image.try(:base64_string)}", width: width )
+  end
 end
--- app/models/webclip.rb
+++ app/models/webclip.rb
@@ -1,4 +1,4 @@
 class Webclip < ActiveRecord::Base
   has_and_belongs_to_many :pages
-  has_one :image
+  belongs_to :image
 end
