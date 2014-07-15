6e4a94fe9cc2a1aa1c1019d1099524437336da29
Ejla <ejla1@me.com>
2014-07-15 17:06:23 +0200

    Navbar helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def li_active_if_current(title, path)    
+    if current_page? path
+      content_tag :li, link_to(title, path), class: "active"
+    else
+      content_tag :li, link_to(title, path)
+    end
+  end
+  
 end
